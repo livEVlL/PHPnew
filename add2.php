da79c4e64c844d439a2b63baf381edc1aff1f143
user <who@where.com>
2016-11-22 17:06:35 +0800

    v1

---
+++ add2.php
@@ -0,0 +1,24 @@
+<?php
+//加密密碼
+$account = $_POST['account'];
+$passwd = $_POST['passwd'];
+$newPass = password_hash($passwd,PASSWORD_DEFAULT);
+//echo $newPass1;
+//
+//$newPass2 = password_hash($password,PASSWORD_DEFAULT);
+//echo $newPass2;
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=iii", "root", "00000000");
+    $stmt = $pdo->prepare("INSERT INTO member(account,passwd) VALUE (?,?)");
+    $stmt->bindParam(1, $account);
+    $stmt->bindParam(2, $newPass);
+    if ($stmt->execute()){
+        echo 'OK';
+    }else{
+        echo 'XX';
+    }
+    }catch (Exception $e){
+    die("Server Busy");
+    }
+
+?>
